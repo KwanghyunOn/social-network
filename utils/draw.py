f211c5fe19835274c797f33bde0c9214e7e421c2
KwanghyunOn <kwanghyun.on@gmail.com>
2020-05-08 08:54:14 +0000

    Test draw.py

--- utils/draw.py
+++ utils/draw.py
@@ -10,10 +10,20 @@ def draw_community(filepath, community_list, savepath):
         for node in community:
             partition[str(node)] = i
 
-    pos = _community_layout(g, partition)
-    f = plt.figure()
-    nx.draw(g, pos, node_color=list(partition.values()), ax=f.add_subplot(111))
-    f.savefig(savepath)
+    size = float(len(set(partition.values())))
+    pos = nx.spring_layout(g)
+
+    count = 0.
+    for com in set(partition.values()):
+        count = count + 1.
+        list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]
+        nx.draw_networkx_nodes(g, pos, list_nodes, node_size=20, node_color=str(count / size))
+    nx.draw_networkx_edges(g, pos, alpha=0.5)
+    plt.savefig(savepath)
+    # f = plt.figure()
+    # pos = _community_layout(g, partition)
+    # nx.draw(g, pos, node_color=list(partition.values()), ax=f.add_subplot(111))
+    # f.savefig(savepath)
 
 
 def _community_layout(g, partition):
